beaa5b6fa0c368bfd7d02b460db7c029d1f28bb0
goldengua <lijiaxuan1111@gmail.com>
2020-11-04 23:13:13 -0600

    Update main.js

--- data_includes/main.js
+++ data_includes/main.js
@@ -92,11 +92,11 @@ Template( variable =>
     //newAudio("tone", variable.AudioFile)
         //.play()
     //,
-    //newImage("pitch_img",variable.ImageFile)
-        //.size(200,200)
-    //,
+    newImage("pitch_img",variable.ImageFile)
+        .size(200,200)
+    ,
     newCanvas("alien",200,200)
-        .add(   0 , 0 , newImage(variable.ImageFile).size(200,200) )
+        .add(   0 , 0 , getImage("pitch_img") )
         .print()
     ,
     newKey('response',"asdjkl")
